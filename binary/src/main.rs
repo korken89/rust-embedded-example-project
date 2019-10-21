8f6a94364452dc74be5309c3728ad9fc04d508a2
Emil Fresk <emil.fresk@gmail.com>
2019-10-21 15:49:46 +0200

    Updated to RTFM 5

--- binary/src/main.rs
+++ binary/src/main.rs
@@ -7,15 +7,20 @@
 pub use firmware as _;
 use nrf52832_hal as _;
 use nrf52832_hal::target;
+// use panic_halt as _;
 use panic_semihosting as _;
 use rtfm::app;
 
-#[app(device = nrf52832_hal::target)]
+#[app(device = nrf52832_hal::target, peripherals = true)]
 const APP: () = {
-    static mut SPI: target::SPIM0 = ();
+    struct Resources {
+        spi: target::SPIM0,
+    }
 
     #[init]
-    fn init() -> init::LateResources {
-        init::LateResources { SPI: device.SPIM0 }
+    fn init(cx: init::Context) -> init::LateResources {
+        init::LateResources {
+            spi: cx.device.SPIM0,
+        }
     }
 };
