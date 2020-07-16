800e00f0cba4ba778aee1f2977f17e73af08c730
Emil Fresk <emil.fresk@gmail.com>
2020-07-16 16:15:05 +0200

    Moved to RTIC, use profile-override for much faster compiles

--- binary/src/main.rs
+++ binary/src/main.rs
@@ -9,7 +9,7 @@ use nrf52832_hal as _;
 use nrf52832_hal::target;
 // use panic_halt as _;
 use panic_semihosting as _;
-use rtfm::app;
+use rtic::app;
 
 #[app(device = nrf52832_hal::target, peripherals = true)]
 const APP: () = {
